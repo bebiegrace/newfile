6e9ba890f463fdd5604ff6fc9cf5d5fd8a71ae78
1styrGroupB <1styrGroupB@pn.local>
2022-06-23 12:35:13 +0800

    save

---
+++ JAB_FASHION_CARE-Website/app.js
@@ -0,0 +1,34 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+hamburger.addEventListener('click', () => {
+	hamburger.classList.toggle('active');
+	mobile_menu.classList.toggle('active');
+});
+
+document.addEventListener('scroll', () => {
+	var scroll_position = window.scrollY;
+	if (scroll_position > 500) {
+		header.style.backgroundColor = 'rgba(80, 74, 71, 0.638)';
+	} else {
+		header.style.backgroundColor = 'rgba(80, 74, 71, 0.638)';
+	}
+});
+
+menu_item.forEach((item) => {
+	item.addEventListener('click', () => {
+		hamburger.classList.toggle('active');
+		mobile_menu.classList.toggle('active');
+	});
+});
+
+
+function myFunction(imgs) {
+  var expandImg = document.getElementById("expandedImg");
+  var imgText = document.getElementById("imgtext");
+  expandImg.src = imgs.src;
+  imgText.innerHTML = imgs.alt;
+  expandImg.parentElement.style.display = "block";
+}
